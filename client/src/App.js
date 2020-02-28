aa19078b8a9495933afeff7c09b35ac2a1ce66c1
Amanda Hogan <ahogan@teamworks.com>
2020-02-28 11:18:19 -0500

    Create login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PrivateRoute from './utils/PrivateRoute';
-
 import Login from "./components/Login";
-import "./styles.scss";
 import BubblePage from "./components/BubblePage";
+import "./styles.scss";
 
 function App() {
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const login = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    axiosWithAuth.post('/api/login', credentials)
+      .then(response => {
+        console.log(response);
+        window.localStorage.setItem('token', response.data);
+        props.history.push('/api/colors');
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
+
+  const handleChange = (event) => {
+    setCredentials({
+      [event.target.name]: event.target.value
+    });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input 
+            id="username"
+            name="username"
+            type="text"
+            value={credentials.username}
+            onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input 
+            id="password"
+            name="password"
+            type="password"
+            value={credentials.password}
+            onChange={handleChange} />
+        </div>
+      </form>
     </>
   );
 };
