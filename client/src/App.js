8057cdc54381e3bd8697c5772b820ed2cd9c0c4f
Amanda Hogan <ahogan@teamworks.com>
2020-02-28 11:11:00 -0500

    Redirect color list to login page unless token specified

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute';
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <nav>
+          <Link to="/api/login">Login</Link>
+          <Link to="/api/colors">Color List</Link>
+        </nav>
+        <Route exact path="/api/login" component={Login} />
+        <PrivateRoute exact path="/api/colors" component={BubblePage} />
       </div>
     </Router>
   );
